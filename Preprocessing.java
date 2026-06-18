2f79e575ebdc213310d062dcb847df235fd53796
unknown <6182201007@student.unpar.ac.id>
2026-06-18 17:21:12 +0700

    memperbaiki stemming

--- Preprocessing.java
+++ Preprocessing.java
@@ -4,7 +4,6 @@ public class Preprocessing {
     private Set<String> stopwords;
 
     public Preprocessing() {
-        // Daftar stopword dasar bahasa Inggris
         stopwords = new HashSet<>(Arrays.asList(
             "a", "an", "and", "are", "as", "at", "be", "but", "by", "for",
             "if", "in", "into", "is", "it", "no", "not", "of", "on", "or",
@@ -32,11 +31,11 @@ public class Preprocessing {
         
 
         if (word.endsWith("ies") && word.length() > 4) {
-            word = word.substring(0, word.length() - 3) + "y"; // parties -> party
+            word = word.substring(0, word.length() - 3) + "y";
         } else if (word.endsWith("es") && !word.endsWith("ces") && !word.endsWith("ses") && word.length() > 3) {
-            word = word.substring(0, word.length() - 2); // boxes -> box
+            word = word.substring(0, word.length() - 2); 
         } else if (word.endsWith("s") && !word.endsWith("ss") && !word.endsWith("us") && word.length() > 2) {
-            word = word.substring(0, word.length() - 1); // cars -> car
+            word = word.substring(0, word.length() - 1);
         }
         
         
